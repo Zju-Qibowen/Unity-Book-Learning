28c11d1a88f611a3d69cdd6195c8c6671652e4c1
qibowen <qibw@zju.edu.cn>
2023-04-13 21:37:34 +0800

    添加敌人的巡逻和场景的NavMesh烘焙

--- Assets/Scripts/EnemyBehaviouor.cs
+++ Assets/Scripts/EnemyBehaviouor.cs
@@ -2,10 +2,50 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviouor : MonoBehaviour
 {
-    
+    //
+    private Transform patrolRoute;
+    //一个List用来存放巡逻的路径点transform
+    public List<Transform> locations;
+    private int locationIndex = 0;
+    private NavMeshAgent agent;
+    private void Start()
+    {
+        patrolRoute = GameObject.Find("Patrol Route").GetComponent<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+        MoveToNextLocation();
+    }
+
+    void MoveToNextLocation()
+    {
+        if (locations.Count == 0)
+        {
+            return;
+        }
+        agent.destination = locations[locationIndex].position;
+        locationIndex = (locationIndex + 1) % locations.Count;
+        Debug.Log($"Heading for location No.{locationIndex+1}");
+    }
+    private void Update()
+    {
+        if (agent.remainingDistance <= 0.2f && !agent.pathPending)
+        {
+            MoveToNextLocation();
+        }
+    }
+
+    void InitializePatrolRoute()
+    {
+        //遍历了叫patrolRoute 的 Transform 组件的所有子物体，并将它们添加到一个名为 locations 的 List<Transform> 中。
+        foreach (Transform transform in patrolRoute)
+        {
+            locations.Add(transform);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
