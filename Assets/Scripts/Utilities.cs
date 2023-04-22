226d6c610dbfd6f0e12aa826aeb47db376a96fcc
qibowen <qibw@zju.edu.cn>
2023-04-22 21:02:02 +0800

    bug fixed

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -6,11 +6,19 @@ public static class Utilities
 {
    public static int playerDeaths = 0;
 
+   public static string UpdateDeathCount(ref int countReference)
+   {
+      countReference += 1;
+      return "Next time you'll be at number " + countReference;
+   }
    public static void RestartLevel()
    {
       SceneManager.LoadScene(0);
       Time.timeScale = 1.0f;
+      Debug.Log("Player deaths:"+playerDeaths);
+      string message = UpdateDeathCount(ref playerDeaths);
+      Debug.Log("Player deaths:"+playerDeaths);
    }
-
+   
 
 }
